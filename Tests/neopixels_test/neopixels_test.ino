d1860c51756fb0421d2faede59dd5cc32eec7a7c
DangerousTim <tpb2k99@gmail.com>
2020-01-28 23:16:47 +0530

    updated neopixel test code

--- Tests/neopixels_test/neopixels_test.ino
+++ Tests/neopixels_test/neopixels_test.ino
@@ -3,28 +3,28 @@
 #include <Adafruit_NeoPixel.h>
 
 // Which pin on the Arduino is connected to the NeoPixels?
-#define PIN        12 // On Trinket or Gemma, suggest changing this to 1
+#define PIN        10 // On Trinket or Gemma, suggest changing this to 1
 
 // How many NeoPixels are attached to the Arduino?
-#define NUMPIXELS 10 // Popular NeoPixel strip size
+#define NUMPIXELS 54 // Popular NeoPixel strip size
 
 // When setting up the NeoPixel library, we tell it how many pixels,
 // and which pin to use to send signals. 
 Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
-#define DELAYVAL 500 // Time (in milliseconds) to pause between pixels
+#define DELAYVAL 50 // Time (in milliseconds) to pause between pixels
 
 void setup() {
   pixels.begin(); // INITIALIZE NeoPixel strip object (REQUIRED)
 
   //keep brightness less to keep power consumtion manageable
-  pixels.setBrightness(192);
+  pixels.setBrightness(255);
 }
 
 void loop() {
   for(int i=0; i<NUMPIXELS; i++) { // For each pixel...
     pixels.clear();
-    pixels.setPixelColor(i, pixels.Color(0, 255, 0));
+    pixels.setPixelColor(i, pixels.Color(random(255), random(255), random(255)));
     pixels.show();   // Send the updated pixel colors to the hardware.
     delay(DELAYVAL); // Pause before next pass through loop
   }
